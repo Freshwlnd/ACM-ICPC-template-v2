0119ae9647f8648f521afcc8e631e61638454605
Freshwlnd <31024123+Freshwlnd@users.noreply.github.com>
2019-02-25 14:15:39 +0800

    Update 二维树状数组.cpp

--- DataStructure/二维树状数组.cpp
+++ DataStructure/二维树状数组.cpp
@@ -1,22 +1,6 @@
 #include <bits/stdc++.h>
-#define per(i,a,n) for (int i=a;i<n;i++)
-#define rep(i,a,n) for (int i=n-1;i>=a;i--)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(),(x).end()
-#define fi first
-#define se second
-#define SZ(x) ((int)(x).size())
-#define mem(a, b) memset(a, b, sizeof(a))
 using namespace std;
 typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pI;
-typedef vector<ll> vI;
-const ll mod(1e9 + 7);
-const ll INF(0x3f3f3f3f);
-ll powmod(ll a, ll b) { ll res = 1; a %= mod; assert(b >= 0); for (; b; b >>= 1) { if (b & 1)res = res * a%mod; a = a * a%mod; }return res; }
-ll gcd(ll a, ll b) { return b ? gcd(b, a%b) : a; }
 // head
 
 const int maxn(1e3 + 7);
@@ -54,9 +38,9 @@ inline int dex(int x1, int y1, int x2, int y2) {
 }
 
 void build_tree() {
-    mem(books, 0);
-    per(i, 1, maxn) {
-        per(j, 1, maxn) {
+    memset(books, 0, sizeof(books));
+    for(int i=1; i < maxn; i++) {
+        for(int j=1; j < maxn; j++) {
             book[i][j] = 1;
             change(i, j, 1);
         }
