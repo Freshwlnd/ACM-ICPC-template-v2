50f89d2d8a7a35957690d9d519b7211441a4bc86
徐徐清风 <freshwind@xuxuqingfengdeMacBook-Pro.local>
2019-07-08 19:33:47 +0800

    增加最大流Dinic算法

---
+++ Algorithm/图论/网络流_最大流_Dinic.cpp
@@ -0,0 +1,108 @@
+//网络流_最大流_Dinic
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF(0x3f3f3f3f);
+
+class Dinic {
+public:
+    int N, S, T;
+    vector<int> head;   //存每个点最后一条边
+    vector<int> depth;  //存每次bfs后每个点的深度
+    vector<int> nxt;    //存每条边的前一条边
+    vector<int> To;     //存每条边连接的另一个点
+    vector<int> Val;    //存每条边的权重
+    int outt;           //存最大流
+
+public:
+    Dinic(int Nn=0, int Ss=0, int Tt=0): N(Nn), S(Ss), T(Tt) {
+        head.resize(N+1);
+        depth.resize(N+1);
+        Val.resize(2);
+        To.resize(2);
+        nxt.resize(2);
+    }
+    void _Add(int u, int v, int w) {
+        nxt.push_back(head[u]);
+        head[u] = To.size();
+        To.push_back(v);
+        Val.push_back(w);
+    }
+    void addEdge(int u, int v, int w) {
+        _Add(u, v, w);
+        _Add(v, u, 0);
+    }
+    int bfs() {
+        queue<int> qq;
+        qq.push(S);
+        vector<int> marked;
+        marked.resize(N+1);
+        marked[S] = 1;
+        depth[T] = depth[S] = 0; 
+        int tmp;
+
+        while(!qq.empty()) {
+            tmp = qq.front();
+            qq.pop();
+            for(int i=head[tmp]; i; i=nxt[i]) {
+                if(Val[i] && !marked[To[i]]) {
+                    marked[To[i]] = 1;
+                    depth[To[i]] = depth[tmp]+1;
+                    qq.push(To[i]);
+                }
+            }
+        }
+        if(marked[T]) {
+            return 1;
+        }
+        return 0;
+    }
+    int dfs(int pos, int val) {
+        if(pos==T) {
+            return val;
+        }
+        for(int i=head[pos]; i; i=nxt[i]) {
+            if(Val[i] && depth[To[i]]==depth[pos]+1) {
+                int valu = dfs(To[i], min(val, Val[i]));
+                if(valu) {
+                    Val[i] -= valu;
+                    Val[i^1] += valu;
+                    return valu;
+                }
+            }
+        }
+        return 0;
+    }
+    void dinic() {
+        outt = 0;
+        while(bfs()) {
+            int val;
+            while((val=dfs(S, INF))) {
+                outt += val;
+            }
+        }
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int N, M, S, T;
+    cin >> N >> M >> S >> T;
+    Dinic DD(N, S, T);
+
+    int u, v, w;
+    for(int i=0; i<M; i++) {
+        cin >> u >> v >> w;
+        DD.addEdge(u, v, w);
+    }
+
+    DD.dinic();
+
+    cout << DD.outt << endl;
+
+    return 0;
+}
